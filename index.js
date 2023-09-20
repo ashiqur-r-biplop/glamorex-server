601dff1d4e4f2711dad6de41299b7284e9500f6d
Ashiqur Rahman Biplop <biplops22@gmail.com>
2023-09-20 14:03:02 +0600

    added by admin verify and seller verify and genaret api seller product

--- index.js
+++ index.js
@@ -43,6 +43,8 @@ const verifyJWT = (req, res, next) => {
     next();
   });
 };
+// instructor
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
@@ -53,6 +55,35 @@ async function run() {
       .collection("product-collection");
     // Send a ping to confirm a successful connection
     // payment system
+    const verifySeller = async (req, res, next) => {
+      const email = req.decoded;
+      // console.log(email);
+      const query = { email: email.email };
+      // console.log(query, "email");
+      const user = await userCollection.findOne(query);
+      // console.log(user);
+      if (user?.userRole !== "seller") {
+        return res
+          .status(403)
+          .send({ error: true, message: "forbidden message" });
+      }
+      next();
+    };
+    const verifyAdmin = async (req, res, next) => {
+      const email = req.decoded;
+      // console.log(email);
+      const query = { email: email.email };
+      // console.log(query, "email");
+      const user = await userCollection.findOne(query);
+      // console.log(user);
+      if (user?.userRole !== "admin") {
+        return res
+          .status(403)
+          .send({ error: true, message: "forbidden message" });
+      }
+      next();
+    };
+
     app.post("/create-payment-intent", verifyJWT, async (req, res) => {
       const price = req.body.price;
       if (price) {
@@ -86,7 +117,8 @@ async function run() {
       const result = await userCollection.insertOne(user);
       res.send(result);
     });
-    app.post("/add-product", verifyJWT, async (req, res) => {
+    // All product added by data base
+    app.post("/add-product", verifyJWT, verifySeller, async (req, res) => {
       const { product } = req.body;
       const newProduct = {
         ...product,
@@ -99,6 +131,13 @@ async function run() {
 
       const result = await productCollection.insertOne(newProduct);
       res.send(result);
+    });
+    // get seller product by useing query seller email
+    app.get("/get-my-products", verifyJWT, verifySeller, async (req, res) => {
+      const email = req.query.email;
+      const query = { seller_email: email };
+      const filter = await productCollection.find(query).toArray();
+      res.send(filter);
     });
     // get account page
     app.get("/account/:email", async (req, res) => {
@@ -138,5 +177,5 @@ app.get("/", async (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`CreativaDesignHub is sitting on port ${port}`);
+  console.log(`Glamorex is sitting on port ${port}`);
 });
